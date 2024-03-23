43985937b2b79be65644e782f0dd3e6c946a90ec
Mohamed Hassan <exorblack@gmail.com>
2024-03-23 16:27:48 +0200

    after last

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Navigations from "./(navbar)/page";
-import Firstsection from "./(first-section)/page";
-import Carousel from "./(curs)/page";
-import Featuers from "./(featuers)/page";
-import Price from "./(pricing)/page";
-import Start from "./(contact)/page";
-import Footer from "./(footer)/page";
+import Navigations from "./navbar/page";
+import Firstsection from "./first-section/page";
+import Carousel from "./curs/page";
+import Featuers from "./featuers/page";
+import Price from "./pricing/page";
+import Start from "./contact/page";
+import Footer from "./footer/page";
 
 export default function Home() {
   return (
